a3da3e701e3119ec915aec9bcd83b4195f05c422
Dave Revell <dave@urbanairship.com>
2012-06-28 17:32:37 -0700

    Add missing files from callback-based error handling

---
+++ src/main/java/com/urbanairship/datacube/dbharnesses/AfterExecute.java
@@ -0,0 +1,8 @@
+package com.urbanairship.datacube.dbharnesses;
+
+import com.urbanairship.datacube.Batch;
+import com.urbanairship.datacube.Op;
+
+public interface AfterExecute<T extends Op> {
+    public void afterExecute(Throwable t);
+}
---
+++ src/test/java/com/urbanairship/datacube/ErrorHandlingTest.java
@@ -0,0 +1,66 @@
+package com.urbanairship.datacube;
+
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableNotFoundException;
+import org.apache.hadoop.hbase.client.HTablePool;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.urbanairship.datacube.DbHarness.CommitType;
+import com.urbanairship.datacube.bucketers.StringToBytesBucketer;
+import com.urbanairship.datacube.dbharnesses.HBaseDbHarness;
+import com.urbanairship.datacube.idservices.CachingIdService;
+import com.urbanairship.datacube.idservices.MapIdService;
+import com.urbanairship.datacube.ops.LongOp;
+
+/**
+ * Make sure database errors are handled
+ */
+public class ErrorHandlingTest extends EmbeddedClusterTestAbstract {
+    @Test
+    public void test() throws Exception {
+        IdService idService = new CachingIdService(5, new MapIdService());
+
+        Configuration conf = getTestUtil().getConfiguration();
+        HTablePool pool = new HTablePool(conf, Integer.MAX_VALUE);
+        
+        DbHarness<LongOp> dbHarness = new HBaseDbHarness<LongOp>(pool, "XY".getBytes(), 
+                "nonexistentTable".getBytes(), "nonexistentCf".getBytes(), LongOp.DESERIALIZER, 
+                idService, CommitType.INCREMENT, 5, 2, 2, null);
+        
+        DataCube<LongOp> cube;
+        
+        Dimension<String> zipcode = new Dimension<String>("zipcode", new StringToBytesBucketer(), 
+                true, 5);
+
+        Rollup zipRollup = new Rollup(zipcode);
+        
+        List<Dimension<?>> dimensions =  ImmutableList.<Dimension<?>>of(zipcode);
+        List<Rollup> rollups = ImmutableList.of(zipRollup);
+        
+        cube = new DataCube<LongOp>(dimensions, rollups);
+
+        DataCubeIo<LongOp> dataCubeIo = new DataCubeIo<LongOp>(cube, dbHarness, 1, Long.MAX_VALUE, 
+                SyncLevel.BATCH_ASYNC);
+        
+        dataCubeIo.writeAsync(new LongOp(1), new WriteBuilder(cube)
+            .at(zipcode, "97212"));
+
+        dataCubeIo.writeAsync(new LongOp(1), new WriteBuilder(cube)
+            .at(zipcode, "97212"));
+
+        dataCubeIo.flush();
+
+        try {
+            dataCubeIo.writeAsync(new LongOp(1), new WriteBuilder(cube)
+                .at(zipcode, "97212"));
+            Assert.fail("Cube should not have accepted more writes after an error!");
+        } catch (AsyncException e) {
+            // This exception *should* happen. Because we wrote to a nonexistent table.
+            Assert.assertTrue(e.getCause().getCause().getCause() instanceof TableNotFoundException);
+        }
+    }
+}
